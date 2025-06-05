dee36eef04cf4e2810bbf2332f4bc4d52699c213
minkhant-200742 <dev7@beedatamyanmar.com>
2025-06-05 09:34:10 +0630

    Day 6

---
+++ ControlStructure/bitwise.py
@@ -0,0 +1,16 @@
+def getTrue():
+    print("Get True")
+    return True
+
+def getFalse():
+    print("Get False")
+    return False
+
+print("getTrue & getFalse", getFalse() & getTrue())
+print("getTrue and getFalse", getFalse() and getTrue())
+
+print("getTrue | getTrue", getTrue() | getFalse())
+print("getTrue or getTrue", getTrue() or getFalse())
+
+print('2^2', 2^2)
+print('2 ^ 3', 2 ^3)
